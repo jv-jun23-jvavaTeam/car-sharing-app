c045552480fc47713c88af24579a2b6c586ee9c3
Alexander Parkhomovsky <69048440+Skarbey@users.noreply.github.com>
2023-10-02 14:49:59 +0300

    Add rental entity

---
+++ src/main/java/com/jvavateam/carsharingapp/model/Rental.java
@@ -0,0 +1,40 @@
+package com.jvavateam.carsharingapp.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.time.LocalDate;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+@Data
+@Entity
+@SQLDelete(sql = "UPDATE rental SET is_deleted = true WHERE id = ?")
+@Where(clause = "is_deleted=false")
+@Table(name = "rentals")
+public class Rental {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private LocalDate rentalDate;
+    private LocalDate returnDate;
+    private LocalDate actualReturnDate;
+    /*@ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "car_id", nullable = false)
+    @EqualsAndHashCode.Exclude
+    @ToString.Exclude
+    private Car car;
+    @JoinColumn(name = "user_id", nullable = false)
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @EqualsAndHashCode.Exclude
+    @ToString.Exclude
+    private User user;*/
+}
