ab01ab9d1258d00e88e716bcb86ecb6a7929ab63
Ivan Fadieiev <ivanfadieiev@gmail.com>
2023-10-02 19:42:35 +0300

    added password validation pattern

--- src/main/java/com/jvavateam/carsharingapp/dto/user/UserRequestDto.java
+++ src/main/java/com/jvavateam/carsharingapp/dto/user/UserRequestDto.java
@@ -3,11 +3,15 @@ package com.jvavateam.carsharingapp.dto.user;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
-
+import jakarta.validation.constraints.Pattern;
 public record UserRequestDto(@Email @NotBlank String email,
-                             @NotBlank @Size(min = 8) String password,
-                             @NotNull @Size(min = 8) String repeatPassword,
+                             @NotBlank @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[a-zA-Z\\d]{6,}$",
+                                     message = "Minimum eight characters, at least one uppercase"
+                                             + " letter, one lowercase letter and one number")
+                             String password,
+                             @NotNull @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[a-zA-Z\\d]{6,}$",
+                                     message = "Minimum eight characters, at least one uppercase"
+                                             + " letter, one lowercase letter and one number") String repeatPassword,
                              @NotBlank String firstName,
                              @NotBlank String lastName) {
 }
