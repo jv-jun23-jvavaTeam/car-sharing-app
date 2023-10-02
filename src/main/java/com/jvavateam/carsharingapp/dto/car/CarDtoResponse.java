2a8aee91f8f8bf87be0b8c13358e3de2e744e03f
Artem Reizer <artem1707200290@gmail.com>
2023-10-02 17:01:09 +0300

    corrected dtos

--- src/main/java/com/jvavateam/carsharingapp/dto/car/CarDtoResponse.java
+++ src/main/java/com/jvavateam/carsharingapp/dto/car/CarDtoResponse.java
@@ -1,7 +1,6 @@
 package com.jvavateam.carsharingapp.dto.car;
 
 import com.jvavateam.carsharingapp.model.Car;
-
 import java.math.BigDecimal;
 
 public record CarDtoResponse(Long id, String model, String brand, Car.Type type,
