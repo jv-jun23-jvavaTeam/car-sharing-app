6e8317788edbc56d299e8bc0ce2425a6057d8ac4
holsoni <soniakostashchuk@gmail.com>
2023-10-02 17:07:16 +0300

    Added mock service layer

---
+++ src/main/java/com/jvavateam/carsharingapp/service/payment/PaymentServiceImpl.java
@@ -0,0 +1,32 @@
+package com.jvavateam.carsharingapp.service.payment;
+
+import com.jvavateam.carsharingapp.dto.payment.CreatePaymentRequestDto;
+import com.jvavateam.carsharingapp.repository.PaymentRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PaymentServiceImpl implements PaymentService {
+    private PaymentRepository paymentRepository;
+
+    @Override
+    public String getAllForCurrentUser() {
+        return "***All payments for current user***";
+    }
+
+    @Override
+    public String createPayment(CreatePaymentRequestDto requestDto) {
+        return "***Payment successfully created***";
+    }
+
+    @Override
+    public String getAllSuccessfulPayments() {
+        return "***Successful payment redirection***";
+    }
+
+    @Override
+    public String getAllPausedPayments() {
+        return "***Paused payment redirection***";
+    }
+}
