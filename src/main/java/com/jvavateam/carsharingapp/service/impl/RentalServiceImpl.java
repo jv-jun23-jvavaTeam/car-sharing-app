dabe04b09f6601f609e5317506bc28655b9ac747
Alexander Parkhomovsky <69048440+Skarbey@users.noreply.github.com>
2023-10-02 22:01:49 +0300

    RentalServiceImpl rv2.0

--- src/main/java/com/jvavateam/carsharingapp/service/impl/RentalServiceImpl.java
+++ src/main/java/com/jvavateam/carsharingapp/service/impl/RentalServiceImpl.java
@@ -2,17 +2,33 @@ package com.jvavateam.carsharingapp.service.impl;
 
 import com.jvavateam.carsharingapp.dto.rental.CreateRentalDto;
 import com.jvavateam.carsharingapp.dto.rental.RentalResponseDto;
+import com.jvavateam.carsharingapp.model.Car;
+import com.jvavateam.carsharingapp.model.Rental;
+import com.jvavateam.carsharingapp.model.User;
+import com.jvavateam.carsharingapp.repository.CarRepository;
+import com.jvavateam.carsharingapp.repository.UserRepository;
 import com.jvavateam.carsharingapp.service.RentalService;
 import java.util.List;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class RentalServiceImpl implements RentalService {
+    private final CarRepository carRepository;
+    private final UserRepository userRepository;
+
 
     @Override
     public RentalResponseDto create(CreateRentalDto createRentalDto) {
+        Rental rental = new Rental();
+        rental.setRentalDate(createRentalDto.rentalDate());
+        Optional<Car> possibleCar = carRepository.findById(createRentalDto.carId());
+        // or from userService
+        Optional<User> possibleUser = userRepository.findById(createRentalDto.userId());
+        rental.setCar();
+        rental.setUser();
         return null;
     }
 
