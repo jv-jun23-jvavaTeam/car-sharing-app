fbf2dfc4d34ce0b04cc1c192b1f03de8afaa2fa5
Alexander Parkhomovsky <69048440+Skarbey@users.noreply.github.com>
2023-10-02 16:23:19 +0300

    maven check fixed

--- src/main/java/com/jvavateam/carsharingapp/dto/rental/CreateRentalDto.java
+++ src/main/java/com/jvavateam/carsharingapp/dto/rental/CreateRentalDto.java
@@ -1,9 +1,6 @@
 package com.jvavateam.carsharingapp.dto.rental;
 
 import java.time.LocalDate;
-import lombok.EqualsAndHashCode;
-import lombok.Value;
 
-@EqualsAndHashCode(callSuper = true)
 public record CreateRentalDto(LocalDate rentalDate, Long carId, Long userId) {
 }
