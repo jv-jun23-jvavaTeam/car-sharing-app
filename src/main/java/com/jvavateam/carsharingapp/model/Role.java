ed92e1a58a08a26d15266283f3148c8faa03f228
Ivan Fadieiev <ivanfadieiev@gmail.com>
2023-10-02 14:25:29 +0300

    added additional annotations to role entity

--- src/main/java/com/jvavateam/carsharingapp/model/Role.java
+++ src/main/java/com/jvavateam/carsharingapp/model/Role.java
@@ -2,6 +2,8 @@ package com.jvavateam.carsharingapp.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -15,7 +17,8 @@ public class Role {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(nullable = false)
+    @Column(nullable = false, unique = true)
+    @Enumerated(EnumType.STRING)
     private RoleName name;
 
     public enum RoleName {
