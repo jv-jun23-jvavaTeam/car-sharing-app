5e760bda7cc66e0e2a94e7ec7d2a2a3eaa75aaee
Alexander Parkhomovsky <69048440+CodeWebWeaver@users.noreply.github.com>
2023-10-02 20:09:07 +0300

    Add rental entity (#3)
    * Add rental entity
    
    * Add rental entity
    
    * isDeleted variable implemented
    
    * maven check fixed
    
    * user updated
    
    * rental car uncommented
    
    ---------
    
    Co-authored-by: Alexander Parkhomovsky <69048440+Skarbey@users.noreply.github.com>

---
+++ src/main/java/com/jvavateam/carsharingapp/model/Rental.java
@@ -0,0 +1,46 @@
+package com.jvavateam.carsharingapp.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.time.LocalDate;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+@Data
+@Entity
+@SQLDelete(sql = "UPDATE rental SET is_deleted = true WHERE id = ?")
+@Where(clause = "is_deleted=false")
+@Table(name = "rentals")
+public class Rental {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "renatal_date", nullable = false)
+    private LocalDate rentalDate;
+    @Column(name = "return_date", nullable = false)
+    private LocalDate returnDate;
+    @Column(name = "actual_return_date", nullable = false)
+    private LocalDate actualReturnDate;
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "car_id", nullable = false)
+    @EqualsAndHashCode.Exclude
+    @ToString.Exclude
+    private Car car;
+    @JoinColumn(name = "user_id", nullable = false)
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @EqualsAndHashCode.Exclude
+    @ToString.Exclude
+    private User user;
+    @Column(name = "is_deleted", nullable = false)
+    private Boolean isDeleted = false;
+}
