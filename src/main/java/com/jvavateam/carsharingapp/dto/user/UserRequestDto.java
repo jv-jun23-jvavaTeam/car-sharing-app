fa41c7c2afbd24d5b7189d4ada5cdc7167342f88
Ivan Fadieiev <ivanfadieiev@gmail.com>
2023-10-02 19:46:23 +0300

    fixed checkstyle misktake

--- src/main/java/com/jvavateam/carsharingapp/dto/user/UserRequestDto.java
+++ src/main/java/com/jvavateam/carsharingapp/dto/user/UserRequestDto.java
@@ -4,14 +4,23 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
+
 public record UserRequestDto(@Email @NotBlank String email,
-                             @NotBlank @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[a-zA-Z\\d]{6,}$",
-                                     message = "Minimum eight characters, at least one uppercase"
-                                             + " letter, one lowercase letter and one number")
+                             @NotBlank @Pattern(regexp = "^(?=.*[a-z])"
+                                     + "(?=.*[A-Z])(?=.*\\d)"
+                                     + "[a-zA-Z\\d]{6,}$",
+                                     message = "Minimum eight characters, "
+                                             + "at least one uppercase"
+                                             + " letter, one lowercase "
+                                             + "letter and one number")
                              String password,
-                             @NotNull @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[a-zA-Z\\d]{6,}$",
-                                     message = "Minimum eight characters, at least one uppercase"
-                                             + " letter, one lowercase letter and one number") String repeatPassword,
+                             @NotNull @Pattern(regexp = "^(?=.*[a-z])"
+                                     + "(?=.*[A-Z])(?=.*\\d)"
+                                     + "[a-zA-Z\\d]{6,}$",
+                                     message = "Minimum eight characters, "
+                                             + "at least one uppercase"
+                                             + " letter, one lowercase "
+                                             + "letter and one number") String repeatPassword,
                              @NotBlank String firstName,
                              @NotBlank String lastName) {
 }
