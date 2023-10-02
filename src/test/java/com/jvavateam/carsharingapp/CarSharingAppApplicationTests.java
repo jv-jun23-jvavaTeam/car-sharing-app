5a141179f2f1cd27fbf9a44ddaecfe25f9538b9e
Dmytro Martyshchuk <Di.mart.ap@gmail.com>
2023-10-02 13:40:12 +0300

    Init commit

---
+++ src/test/java/com/jvavateam/carsharingapp/CarSharingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jvavateam.carsharingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarSharingAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
