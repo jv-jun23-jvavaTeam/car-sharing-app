4e5436c9c27b4b820ccc75fc32d3ca8ff12c2d17
Artem Reizer <artem1707200290@gmail.com>
2023-10-02 18:29:21 +0300

    fixed checkstyle mistake

--- src/main/java/com/jvavateam/carsharingapp/dto/car/CarDtoRequest.java
+++ src/main/java/com/jvavateam/carsharingapp/dto/car/CarDtoRequest.java
@@ -4,7 +4,6 @@ import com.jvavateam.carsharingapp.model.Car;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
-
 import java.math.BigDecimal;
 
 public record CarDtoRequest(@NotNull String model,
