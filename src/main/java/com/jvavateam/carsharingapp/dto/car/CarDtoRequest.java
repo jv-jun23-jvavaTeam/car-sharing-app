682088cbbc2c058ffe6e2b6b5e0ade4d4dcfe203
Artem Reizer <136165867+ArtemReizer@users.noreply.github.com>
2023-10-02 19:11:08 +0300

    Add Car dto (#12)
    * added Car model, liquibase
    
    * added type field in Car model and column in cars table
    
    * added CarDtoRequest and CarDtoResponse
    
    * added annotation and corrected liquibase
    
    * corrected dtos
    
    * added field validation of request dto
    
    * corrected annotation and liquibase naming
    
    * fixed checkstyle mistake

---
+++ src/main/java/com/jvavateam/carsharingapp/dto/car/CarDtoRequest.java
@@ -0,0 +1,14 @@
+package com.jvavateam.carsharingapp.dto.car;
+
+import com.jvavateam.carsharingapp.model.Car;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import java.math.BigDecimal;
+
+public record CarDtoRequest(@NotNull String model,
+                            @NotNull String brand,
+                            @Positive int inventory,
+                            @NotNull @Min(0) BigDecimal dailyFee,
+                            @NotNull Car.Type type) {
+}
