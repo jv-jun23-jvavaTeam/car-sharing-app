8bdd613e777d6fd094f8dc288a03cf4d568f2618
Artem Reizer <artem1707200290@gmail.com>
2023-10-03 14:47:58 +0300

    implemented Car service and added annotation for mapper

--- src/main/java/com/jvavateam/carsharingapp/mapper/car/CarMapper.java
+++ src/main/java/com/jvavateam/carsharingapp/mapper/car/CarMapper.java
@@ -1,9 +1,12 @@
 package com.jvavateam.carsharingapp.mapper.car;
 
+import com.jvavateam.carsharingapp.config.MapperConfiguration;
 import com.jvavateam.carsharingapp.dto.car.CarDtoRequest;
 import com.jvavateam.carsharingapp.dto.car.CarDtoResponse;
 import com.jvavateam.carsharingapp.model.Car;
+import org.mapstruct.Mapper;
 
+@Mapper(config = MapperConfiguration.class)
 public interface CarMapper {
     CarDtoResponse toDto(Car car);
 
