ac2d3b2a5edd88d5c2810f7846d656d1f38a056f
holsoni <soniakostashchuk@gmail.com>
2023-10-03 12:10:08 +0300

    Fixed codestyle errors in imports

--- src/main/java/com/jvavateam/carsharingapp/model/Payment.java
+++ src/main/java/com/jvavateam/carsharingapp/model/Payment.java
@@ -1,7 +1,5 @@
 package com.jvavateam.carsharingapp.model;
 
-import java.math.BigDecimal;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -13,6 +11,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import java.math.BigDecimal;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
