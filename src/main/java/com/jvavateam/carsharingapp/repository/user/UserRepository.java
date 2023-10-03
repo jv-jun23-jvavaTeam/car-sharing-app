3662e9d7f0837c26d5b4d5c93dbcc0886aada688
Ivan Fadieiev <ivanfadieiev@gmail.com>
2023-10-03 16:15:16 +0300

    Added entitygraph for user entity

--- src/main/java/com/jvavateam/carsharingapp/repository/user/UserRepository.java
+++ src/main/java/com/jvavateam/carsharingapp/repository/user/UserRepository.java
@@ -3,11 +3,9 @@ package com.jvavateam.carsharingapp.repository.user;
 import com.jvavateam.carsharingapp.model.User;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    @Query("FROM User u LEFT JOIN FETCH u.roles WHERE u.email = :email")
     Optional<User> findByEmail(String email);
 }
