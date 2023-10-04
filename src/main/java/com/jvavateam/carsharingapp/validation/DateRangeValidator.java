bbf64b1159b04e0dbb00469df02328de52489cc3
Alexander Parkhomovsky <69048440+Skarbey@users.noreply.github.com>
2023-10-04 18:33:04 +0300

    DataRange reflection reworked, repository fixed rv3.0

--- src/main/java/com/jvavateam/carsharingapp/validation/DateRangeValidator.java
+++ src/main/java/com/jvavateam/carsharingapp/validation/DateRangeValidator.java
@@ -2,8 +2,8 @@ package com.jvavateam.carsharingapp.validation;
 
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
-import java.lang.reflect.Field;
 import java.time.LocalDate;
+import org.springframework.beans.BeanWrapperImpl;
 
 public class DateRangeValidator implements ConstraintValidator<DateRange, Object> {
     private static final String RENTAL_DATE_FIELD_NAME = "rentalDate";
@@ -14,20 +14,10 @@ public class DateRangeValidator implements ConstraintValidator<DateRange, Object
         if (dto == null) {
             return true;
         }
-        try {
-            LocalDate rentalDate = (LocalDate) getProperty(dto, RENTAL_DATE_FIELD_NAME);
-            LocalDate returnDate = (LocalDate) getProperty(dto, RETURN_DATE_FIELD_NAME);
-            return rentalDate.isBefore(returnDate);
-        } catch (Exception e) {
-            throw new RuntimeException("Exception during data validation.", e);
-        }
-    }
-
-    private Object getProperty(Object object, String propertyName) throws NoSuchFieldException,
-            IllegalAccessException {
-        Class<?> clazz = object.getClass();
-        Field field = clazz.getDeclaredField(propertyName);
-        field.setAccessible(true);
-        return field.get(object);
+        LocalDate rentalDate = (LocalDate) new BeanWrapperImpl(dto)
+                .getPropertyValue(RENTAL_DATE_FIELD_NAME);
+        LocalDate returnDate = (LocalDate) new BeanWrapperImpl(dto)
+                .getPropertyValue(RETURN_DATE_FIELD_NAME);
+        return rentalDate.isBefore(returnDate);
     }
 }
