e48bffe69f065f62de882c297c5aebb82336e49f
Alexander Parkhomovsky <69048440+Skarbey@users.noreply.github.com>
2023-10-04 14:32:15 +0300

    implemented field validation rv3.0

--- src/main/java/com/jvavateam/carsharingapp/dto/rental/RentalSearchParameters.java
+++ src/main/java/com/jvavateam/carsharingapp/dto/rental/RentalSearchParameters.java
@@ -2,6 +2,8 @@ package com.jvavateam.carsharingapp.dto.rental;
 
 import jakarta.validation.constraints.NotNull;
 
-public record RentalSearchParameters(@NotNull(message = "User Id cannot be empty") Long userId,
-                                     @NotNull(message = "isActive cannot be empty") Boolean isActive) {
+public record RentalSearchParameters(@NotNull(message = "User Id cannot be empty")
+                                     Long userId,
+                                     @NotNull(message = "isActive cannot be empty")
+                                     Boolean isActive) {
 }
