900035ac6a349767270f22045c7bf64bd5bd1888
Ivan Fadieiev <134369531+IvanFadieiev@users.noreply.github.com>
2023-10-04 17:40:06 +0300

    Added password match validator, refactored user dtos (#35)
    * added password match validator, refactored user dtos

--- src/main/java/com/jvavateam/carsharingapp/dto/user/UserLoginRequestDto.java
+++ src/main/java/com/jvavateam/carsharingapp/dto/user/UserLoginRequestDto.java
@@ -1,11 +1,17 @@
 package com.jvavateam.carsharingapp.dto.user;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import org.hibernate.validator.constraints.Length;
 
 public record UserLoginRequestDto(
-        @Email @NotBlank String email,
-        @NotBlank @Length(min = 8)
+        @NotBlank(message = "User's email cannot be blank")
+        @Email
+        @Schema(description = "User's email", example = "user@mail.com")
+        String email,
+        @NotBlank(message = "User's password cannot be blank")
+        @Length(min = 8, message = "Minimum password length is 8")
+        @Schema(description = "User's password", example = "Aa12345678")
         String password) {
 }
--- src/main/java/com/jvavateam/carsharingapp/dto/user/UserRequestDto.java
+++ src/main/java/com/jvavateam/carsharingapp/dto/user/UserRequestDto.java
@@ -1,26 +1,42 @@
 package com.jvavateam.carsharingapp.dto.user;
 
+import com.jvavateam.carsharingapp.validation.FieldMatch;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 
-public record UserRequestDto(@Email @NotBlank String email,
-                             @NotBlank @Pattern(regexp = "^(?=.*[a-z])"
+@FieldMatch(first = "password",
+            second = "repeatPassword",
+            message = "Passwords don't match")
+public record UserRequestDto(@Email
+                             @NotBlank(message = "User's email cannot be blank")
+                             @Schema(description = "User's email", example = "user@mail.com")
+                             String email,
+                             @NotBlank(message = "User's password cannot be blank")
+                             @Pattern(regexp = "^(?=.*[a-z])"
                                      + "(?=.*[A-Z])(?=.*\\d)"
                                      + "[a-zA-Z\\d]{6,}$",
                                      message = "Minimum eight characters, "
                                              + "at least one uppercase"
                                              + " letter, one lowercase "
                                              + "letter and one number")
+                             @Schema(description = "User's password", example = "Aa12345678")
                              String password,
-                             @NotNull @Pattern(regexp = "^(?=.*[a-z])"
+                             @NotBlank(message = "User's repeat password cannot be blank")
+                             @Pattern(regexp = "^(?=.*[a-z])"
                                      + "(?=.*[A-Z])(?=.*\\d)"
                                      + "[a-zA-Z\\d]{6,}$",
                                      message = "Minimum eight characters, "
                                              + "at least one uppercase"
                                              + " letter, one lowercase "
-                                             + "letter and one number") String repeatPassword,
-                             @NotBlank String firstName,
-                             @NotBlank String lastName) {
+                                             + "letter and one number")
+                             @Schema(description = "User's repeat password", example = "Aa12345678")
+                             String repeatPassword,
+                             @NotBlank(message = "User's first name cannot be blank")
+                             @Schema(description = "User's first name", example = "John")
+                             String firstName,
+                             @NotBlank(message = "User's last name cannot be blank")
+                             @Schema(description = "User's last name", example = "Doe")
+                             String lastName) {
 }
