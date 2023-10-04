30351fe0aa1108c63b315bf214469d75164c9b0c
holsoni <soniakostashchuk@gmail.com>
2023-10-04 13:51:09 +0300

    Added swagger annotations to controller

--- src/main/java/com/jvavateam/carsharingapp/controller/PaymentController.java
+++ src/main/java/com/jvavateam/carsharingapp/controller/PaymentController.java
@@ -6,11 +6,14 @@ import com.jvavateam.carsharingapp.dto.payment.PaymentResponseDto;
 import com.jvavateam.carsharingapp.service.payment.PaymentService;
 import com.stripe.exception.StripeException;
 import java.util.List;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -20,35 +23,43 @@ public class PaymentController {
     private final PaymentService paymentService;
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    @Operation(summary = "Create new payment session",
+            description = "Create new payment session")
     public PaymentResponseDto createPayment(CreatePaymentRequestDto requestDto)
             throws StripeException {
         return paymentService.createPayment(requestDto);
     }
 
     @GetMapping
+    @Operation(summary = "Retrieving all payments",
+            description = "Retrieving all payments for current user")
     public List<PaymentResponseDto> getAll() {
         return paymentService.getAllForCurrentUser();
     }
 
     @GetMapping("/paused")
+    @Operation(summary = "Retrieving all paused payments",
+            description = "Retrieving all paused payments for current user")
     public List<PaymentResponseDto> getAllSuccessfulPayments() {
         return paymentService.getAllSuccessfulPayments();
     }
 
     @GetMapping("/successful")
+    @Operation(summary = "Retrieving all successful payments",
+            description = "Retrieving all successful payments for current user")
     public List<PaymentResponseDto> getAllPausedPayments() {
         return paymentService.getAllPausedPayments();
     }
 
     @GetMapping("/successPayment")
-    public PaymentOperationMessage successful(@RequestParam String sessionId,
-                                              CreatePaymentRequestDto requestDto) {
+    public PaymentOperationMessage successful(@RequestParam String sessionId) {
         paymentService.updatePaymentStatus(sessionId);
         return new PaymentOperationMessage("Payment " + sessionId + " successfully provided!");
     }
 
     @GetMapping("/cancel")
-    public PaymentOperationMessage error(CreatePaymentRequestDto requestDto) {
+    public PaymentOperationMessage error() {
         return new PaymentOperationMessage("Payment cancelled! You can try again later. "
                 + "The session will be open for 24hours.");
     }
--- src/main/java/com/jvavateam/carsharingapp/repository/payment/PaymentRepository.java
+++ src/main/java/com/jvavateam/carsharingapp/repository/payment/PaymentRepository.java
@@ -3,13 +3,14 @@ package com.jvavateam.carsharingapp.repository.payment;
 import com.jvavateam.carsharingapp.model.Payment;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
-    //@Query("FROM Payment p join fetch Rental r WHERE r.user.id = ?#{ principal?.id}"
-    // "AND p.status = :status")
+    @Query("FROM Payment p join fetch Rental r WHERE r.user.id = ?#{ principal?.id}"
+                + "AND p.status = :status")
     List<Payment> findAllByStatus(Payment.Status status);
 
-    // @Query("FROM Payment p join fetch Rental r WHERE r.user.id = ?#{principal?.id}")
+    @Query("FROM Payment p join fetch Rental r WHERE r.user.id = ?#{principal?.id}")
     List<Payment> findAll();
 
     Payment findBySessionId(String sessionId);
