4fbdac53e5c64ebab33e9dba2082b604252cd7f4
Alexander Parkhomovsky <69048440+CodeWebWeaver@users.noreply.github.com>
2023-10-05 18:37:19 +0300

    Implemented Rental tests for Service, Repository, Controller layers (#48)
    * granted access for manager to other endpoints rv3.0
    
    * rentalDate included in request DTO, mapper rework rv3.0
    
    * rentalDate included in request DTO, mapper rework rv3.0 (#40)
    
    Co-authored-by: Alexander Parkhomovsky <69048440+Skarbey@users.noreply.github.com>
    
    * rentalDate included in request DTO, mapper rework rv3.0
    
    * rental date reflection validation implemented rv3.0
    
    * rental date reflection validation implemented rv3.0
    
    * settled tests fundament
    
    * service mock tests implemented rv4.0
    
    * RentalServiceTest with valid Data implemented rv4.0
    
    * RentalServiceTest with valid Data implemented rv4.0
    
    * RentalRepositoryTest implemented with error rv4.0
    
    * RentalRepositoryTest implemented with error rv4.0
    
    * implemented tests for RentalController and RentalRepository rv4.0
    
    * implemented tests for RentalController rv4.0
    
    * refactored scripts naming rv4.0
    
    * refactored scripts naming rv4.0
    
    * refactored scripts naming rv4.0
    
    * docker setup
    
    * docker test associated
    
    * scripts reworked
    
    * docker setup
    
    * id roles rework
    
    * roles insertion fix
    
    * Start setup Postman collection rental endpoints
    
    ---------
    
    Co-authored-by: Alexander Parkhomovsky <69048440+Skarbey@users.noreply.github.com>

--- src/main/java/com/jvavateam/carsharingapp/model/Car.java
+++ src/main/java/com/jvavateam/carsharingapp/model/Car.java
@@ -10,6 +10,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import java.math.BigDecimal;
 import lombok.Data;
+import lombok.experimental.Accessors;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.Where;
 
@@ -18,6 +19,7 @@ import org.hibernate.annotations.Where;
 @SQLDelete(sql = "UPDATE cars SET is_deleted = true WHERE id = ?")
 @Where(clause = "is_deleted = false")
 @Table(name = "cars")
+@Accessors(chain = true)
 public class Car {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/main/java/com/jvavateam/carsharingapp/model/Rental.java
+++ src/main/java/com/jvavateam/carsharingapp/model/Rental.java
@@ -13,6 +13,7 @@ import java.time.LocalDate;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
+import lombok.experimental.Accessors;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.Where;
 
@@ -21,6 +22,7 @@ import org.hibernate.annotations.Where;
 @SQLDelete(sql = "UPDATE rental SET is_deleted = true WHERE id = ?")
 @Where(clause = "is_deleted=false")
 @Table(name = "rentals")
+@Accessors(chain = true)
 public class Rental {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
