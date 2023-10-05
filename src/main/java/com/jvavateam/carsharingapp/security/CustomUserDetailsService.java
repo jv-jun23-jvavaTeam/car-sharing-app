91755a08e5dfa62d17e75992f6ac10cf45b5103b
Dmytro Martyshchuk <Di.mart.ap@gmail.com>
2023-10-05 21:12:44 +0300

    Fixed issues login endpoint

--- src/main/java/com/jvavateam/carsharingapp/security/CustomUserDetailsService.java
+++ src/main/java/com/jvavateam/carsharingapp/security/CustomUserDetailsService.java
@@ -1,6 +1,5 @@
 package com.jvavateam.carsharingapp.security;
 
-import com.jvavateam.carsharingapp.exception.EntityNotFoundException;
 import com.jvavateam.carsharingapp.repository.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -14,9 +13,10 @@ public class CustomUserDetailsService implements UserDetailsService {
     private final UserRepository userRepository;
 
     @Override
-    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        return userRepository.findByEmail(email).orElseThrow(
-                () -> new EntityNotFoundException("User with such "
-                        + "email does not exists!"));
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByEmail(username)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException("User not found by username: " + username)
+                );
     }
 }
