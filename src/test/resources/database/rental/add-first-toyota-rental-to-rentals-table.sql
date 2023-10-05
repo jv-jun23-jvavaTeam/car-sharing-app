08aabaf9adc2df27413910d1b150e550c2cf53f3
holsoni <soniakostashchuk@gmail.com>
2023-10-05 16:47:22 +0300

    Added scripts for tests

---
+++ src/test/resources/database/rental/add-first-toyota-rental-to-rentals-table.sql
@@ -0,0 +1,2 @@
+INSERT INTO rentals (id, rental_date, return_date, actual_return_date, car_id, user_id, is_active, is_deleted)
+VALUES (100, '2023-11-05', '2023-11-20', '2023-11-20', 100, 100, true, false);
