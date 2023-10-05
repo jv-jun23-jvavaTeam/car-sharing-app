605f9c4d0f203c5ac4f5fd013064f766d41452c9
Ivan Fadieiev <ivanfadieiev@gmail.com>
2023-10-05 10:07:52 +0300

    added tests for user repository

---
+++ src/test/java/com/jvavateam/carsharingapp/repository/UserRepositoryTest.java
@@ -0,0 +1,91 @@
+package com.jvavateam.carsharingapp.repository;
+
+import com.jvavateam.carsharingapp.config.SpringSecurityTestConfig;
+import com.jvavateam.carsharingapp.model.User;
+import com.jvavateam.carsharingapp.repository.user.UserRepository;
+import java.util.Optional;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.test.context.jdbc.Sql;
+
+@DataJpaTest
+@Import(SpringSecurityTestConfig.class)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class UserRepositoryTest {
+    private static final User DENZEL = new User()
+            .setEmail("Denzel@mail.com")
+            .setPassword("Denzel123456")
+            .setFirstName("Denzel")
+            .setLastName("Denzel");
+    private static final User JOHN = new User()
+            .setId(1L)
+            .setEmail("john@mail.com")
+            .setPassword("John1234")
+            .setFirstName("John")
+            .setLastName("Smith");
+    private static final String INVALID_EMAIL = "invalid@mail.com";
+    private static final String JOHN_EMAIL = "john@mail.com";
+    private static final String CLEAR_USERS_TABLE =
+            "classpath:database/scripts/user/clear-users-table.sql";
+    private static final String ADD_USER =
+            "classpath:database/scripts/user/add-user-to-users-table.sql";
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    @DisplayName("Verify findByEmail() method works")
+    @Sql(scripts = {
+            CLEAR_USERS_TABLE
+    }, executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+    public void findByEmail_ValidEmail_ReturnsOptionalOfUser() {
+        userRepository.save(DENZEL);
+        Optional<User> userFromDB = userRepository.findByEmail(DENZEL.getEmail());
+        Assertions.assertTrue(userFromDB.isPresent());
+        Assertions.assertEquals(userFromDB.get(), DENZEL);
+    }
+
+    @Test
+    @DisplayName("Verify findByEmail() method works")
+    @Sql(scripts = {
+            CLEAR_USERS_TABLE
+    }, executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+    public void findByEmail_InValidEmail_ReturnsValidUser() {
+        userRepository.save(DENZEL);
+        Optional<User> userFromDB = userRepository.findByEmail(INVALID_EMAIL);
+        Assertions.assertTrue(userFromDB.isEmpty());
+    }
+
+    @Test
+    @WithUserDetails(JOHN_EMAIL)
+    @DisplayName("Verify getCurrentUser() method works")
+    @Sql(scripts = {
+            ADD_USER
+    }, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+    @Sql(scripts = {
+            CLEAR_USERS_TABLE
+    }, executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+    public void getCurrentUser_ValidUser_ReturnsTrue() {
+        User currentUser = userRepository.getCurrentUser();
+        Assertions.assertEquals(currentUser, JOHN);
+    }
+
+    @Test
+    @WithUserDetails(JOHN_EMAIL)
+    @DisplayName("Verify getCurrentUser() method doesn't work")
+    @Sql(scripts = {
+            ADD_USER
+    }, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+    @Sql(scripts = {
+            CLEAR_USERS_TABLE
+    }, executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+    public void getCurrentUser_InValidUser_ReturnsFalse() {
+        User currentUser = userRepository.getCurrentUser();
+        Assertions.assertNotEquals(currentUser, DENZEL);
+    }
+}
---
+++ src/test/resources/database/scripts/user/add-user-to-users-table.sql
@@ -0,0 +1,2 @@
+INSERT INTO users (id, email, first_name, last_name, password, is_deleted)
+VALUES (1, 'john@mail.com', 'John', 'Smith', 'John1234', false);
