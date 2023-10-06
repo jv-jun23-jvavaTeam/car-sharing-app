6e279d231201197c58201a1dd7e2ca5567da2eb5
Alexander Parkhomovsky <69048440+CodeWebWeaver@users.noreply.github.com>
2023-10-06 11:37:35 +0300

    implemented rental fix getALl method (#63)
    Co-authored-by: Alexander Parkhomovsky <69048440+Skarbey@users.noreply.github.com>

--- src/main/java/com/jvavateam/carsharingapp/repository/rental/RentalRepository.java
+++ src/main/java/com/jvavateam/carsharingapp/repository/rental/RentalRepository.java
@@ -4,6 +4,7 @@ import com.jvavateam.carsharingapp.model.Rental;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
@@ -18,7 +19,14 @@ public interface RentalRepository extends JpaRepository<Rental, Long>,
             WHERE r.id = :id
             AND r.user.id = ?#{principal?.id}
             """)
-    Optional<Rental> getByIdForCurrentUser(Long id);
+    Optional<Rental> findByIdForCurrentUser(Long id);
+
+    @EntityGraph(attributePaths = {"car", "user"})
+    @Query("""
+            FROM Rental r
+            WHERE r.user.id = ?#{principal?.id}
+            """)
+    List<Rental> findAllForCurrentUser(Pageable pageable);
 
     @EntityGraph(attributePaths = {"car", "user"})
     @Query("""
