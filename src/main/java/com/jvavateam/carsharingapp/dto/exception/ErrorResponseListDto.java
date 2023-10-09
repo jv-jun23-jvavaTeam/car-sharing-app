c4c29c7190e2b4068de8846a29fede12654b7134
Alexander Parkhomovsky <69048440+Skarbey@users.noreply.github.com>
2023-10-09 12:07:50 +0300

    record exception dto provided

--- src/main/java/com/jvavateam/carsharingapp/dto/exception/ErrorResponseListDto.java
+++ src/main/java/com/jvavateam/carsharingapp/dto/exception/ErrorResponseListDto.java
@@ -6,7 +6,7 @@ import lombok.experimental.Accessors;
 import org.springframework.http.HttpStatus;
 
 @Accessors(chain = true)
-public record ErrorResponseListDto (LocalDateTime timestamp,
+public record ErrorResponseListDto(LocalDateTime timestamp,
                                    HttpStatus status,
                                    List<String> validationErrors) {
 }
